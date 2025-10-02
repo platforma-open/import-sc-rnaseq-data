c19b1ad55a493147fb1ab9eeb535d8fc93f86666
aiturbidemil <aiturbide@milaboratories.com>
2025-10-02 12:24:09 +0200

    boilerplate

---
+++ model/src/index.ts
@@ -0,0 +1,18 @@
+import type { InferOutputsType } from '@platforma-sdk/model';
+import { BlockModel } from '@platforma-sdk/model';
+
+export type BlockArgs = {
+  name?: string;
+};
+
+export const model = BlockModel.create()
+
+  .withArgs<BlockArgs>({})
+
+  .output('tengoMessage', (ctx) => ctx.outputs?.resolve('tengoMessage')?.getDataAsJson())
+
+  .sections((_ctx) => [{ type: 'link', href: '/', label: 'Main' }])
+
+  .done();
+
+export type BlockOutputs = InferOutputsType<typeof model>;
