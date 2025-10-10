618be35915c9efabafe9d5ff50ab842502ada23c
aiturbidemil <aiturbide@milaboratories.com>
2025-10-10 10:07:28 +0200

    WIP: move changes to new branch

--- model/src/index.ts
+++ model/src/index.ts
@@ -1,18 +1,55 @@
-import type { InferOutputsType } from '@platforma-sdk/model';
-import { BlockModel } from '@platforma-sdk/model';
+import type { InferOutputsType, PlDataTableStateV2, PlRef } from '@platforma-sdk/model';
+import { BlockModel, createPlDataTableStateV2, isPColumnSpec } from '@platforma-sdk/model';
 
 export type BlockArgs = {
+  datasetRef?: PlRef;
   name?: string;
+};
+
+export type UiState = {
+  tableState: PlDataTableStateV2;
+  title: string;
+  settingsOpen: boolean;
 };
 
 export const model = BlockModel.create()
 
   .withArgs<BlockArgs>({})
 
-  .output('tengoMessage', (ctx) => ctx.outputs?.resolve('tengoMessage')?.getDataAsJson())
+  .withUiState<UiState>({
+    tableState: createPlDataTableStateV2(),
+    title: 'Import GEX Data',
+    settingsOpen: true,
+  })
+
+  .argsValid((ctx) => {
+    const { datasetRef } = ctx.args;
+    if (datasetRef === undefined) return false;
+
+    return true;
+  })
+
+  .output('datasetOptions', (ctx) => {
+    return ctx.resultPool.getOptions((v) => {
+      if (!isPColumnSpec(v)) return false;
+      const domain = v.domain;
+      return (
+        v.name === 'pl7.app/sequencing/data'
+        && (v.valueType as string) === 'File'
+        && domain !== undefined
+        && (domain['pl7.app/fileExtension'] === 'csv'
+          || domain['pl7.app/fileExtension'] === 'csv.gz'
+          || domain['pl7.app/fileExtension'] === 'tsv'
+          || domain['pl7.app/fileExtension'] === 'tsv.gz')
+      );
+    },
+    );
+  })
 
   .sections((_ctx) => [{ type: 'link', href: '/', label: 'Main' }])
 
-  .done();
+  .title((ctx) => ctx.uiState.title)
+
+  .done(2);
 
 export type BlockOutputs = InferOutputsType<typeof model>;
--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,15 +1,51 @@
 <script setup lang="ts">
-import { PlAlert, PlBlockPage, PlTextField } from '@platforma-sdk/ui-vue';
+import { PlBlockPage, PlBtnGhost, PlDropdownRef, PlMaskIcon24, PlSlideModal } from '@platforma-sdk/ui-vue';
 import { useApp } from '../app';
+import { plRefsEqual, type PlRef } from '@platforma-sdk/model';
 
 const app = useApp();
+
+function onModalUpdate(val: boolean) {
+  const mustStayOpen = app.model.args.datasetRef === undefined;
+  if (mustStayOpen) {
+    app.model.ui.settingsOpen = true;
+    return;
+  }
+  app.model.ui.settingsOpen = val;
+}
+
+const setDataset = (datasetRef: PlRef | undefined) => {
+  app.model.args.datasetRef = datasetRef;
+  if (datasetRef)
+    app.model.ui.title = 'Import GEX Data - ' + app.model.outputs.datasetOptions?.find((o) => plRefsEqual(o.ref, datasetRef))?.label;
+};
+
 </script>
 
 <template>
   <PlBlockPage>
-    <PlTextField v-model="app.model.args.name" label="Enter your name" :clearable="() => undefined" />
+    <template #title>
+      {{ app.model.ui.title }}
+    </template>
+    <template #append>
+      <PlBtnGhost @click.stop="() => (app.model.ui.settingsOpen = true)">
+        Settings
+        <template #append>
+          <PlMaskIcon24 name="settings" />
+        </template>
+      </PlBtnGhost>
+    </template>
 
-    <PlAlert v-if="app.model.outputs.tengoMessage" type="success"> {{ app.model.outputs.tengoMessage }} </PlAlert>
-
+    <PlSlideModal :model-value="app.model.ui.settingsOpen" @update:model-value="onModalUpdate">
+      <template #title>Settings</template>
+      <PlDropdownRef
+        v-model="app.model.args.datasetRef"
+        :options="app.model.outputs.datasetOptions"
+        label="Select dataset"
+        clearable
+        required
+        @update:model-value="setDataset"
+      />
+    </PlSlideModal>
   </PlBlockPage>
 </template>
